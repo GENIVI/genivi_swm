77df4c5d9203f89d52580275af84d87bf5b7e3bf
Magnus Feuer <mfeuer@jaguarlandrover.com>
2015-10-25 19:41:16 -0700

    Startup script

---
+++ start_swm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# (c) 2015 - Jaguar Land Rover.
+#
+# Mozilla Public License 2.0
+#
+# Ubuntu Software Manager Loader launch script
+#
+
+
+# We use xterm since there is no way of putting gnome-terminal in the
+# foreground.
+xterm -T "Package Manager" -e python package_manager.py &
+xterm -T "Partition Manager" -e python partition_manager.py &
+xterm -T "ECU1 Module Loader" -e python ecu1_module_loader.py &
+xterm -T "Software Loading Manager" -e python software_loading_manager.py
+
+echo "Press Ctrl-C shut down Software Manager"
