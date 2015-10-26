19bc5693fc469131fc47e2221dbc9f108bd6b542
Magnus Feuer <mfeuer@jaguarlandrover.com>
2015-10-25 19:03:36 -0700

    Simplified SLM to directly forward package process operations to their package, partition, or module loader manager

--- software_loading_manager.py
+++ software_loading_manager.py
@@ -17,169 +17,6 @@ hmi = None
 dbus_name="org.genivi.software_loading_manager"
 
             
-
-#
-# PackageProcessingBase, inherited by the package manager, the
-# partition manager, and all module loaders, provides the
-# class process() method used to execute all package operations.
-#
-# Process will look at target and call the correct subclass's
-# process_impl() method to forward the package operation
-# to the correct python process.
-#
-class PackageProcessingBase(dbus.service.Object):
-    instances = {}
-
-    #
-    # Retrieve the subclass instance registered with
-    # a name matching 'target' argument and invoke
-    # its process_impl() metnod.
-    # The sublcass (PackageProcessingBase, ECU1ModuleLoaderProcessor, etc)
-    # and its process_impl() will forward the package operation to the
-    # correct DBUS object (package_manager.py, ecu1_loading_mnager.py, etc),
-    # and return the result.
-    #
-    @staticmethod
-    def process(path,
-                package_id,
-                major, 
-                minor, 
-                patch, 
-                command,
-                size, 
-                description,
-                vendor,
-                target): 
-        print "process(): {}".format(target)
-        print "process(): {}".format(PackageProcessingBase.instances)
-        if target not in PackageProcessingBase.instances:
-            print "PackageProcessingBase.process({}): Target not found"
-            return (99, "Unknown target: {}".format(target))
-            
-        process = PackageProcessingBase.instances[target]
-        print "process: found {}".format(type(process).__name__)
-        return process.process_impl(path,
-                                    package_id,
-                                    major, 
-                                    minor, 
-                                    patch, 
-                                    command,
-                                    size, 
-                                    description,
-                                    vendor,
-                                    target)
-
-
-    def __init__(self, name):
-        self.bus = dbus.SessionBus()
-        self.name = name
-        self.slm_bus_name = dbus.service.BusName(dbus_name, bus=self.bus)
-        print "Base class constructor from {}: {}".format(type(self).__name__, name)
-        PackageProcessingBase.instances[name] = self
-        self.module_loader_bus_name = dbus.service.BusName(dbus_name,
-                                                           bus=self.bus)
-
-        dbus.service.Object.__init__(self, 
-                                     self.slm_bus_name, 
-                                     "/org/genivi/software_loading_manager" + self.name)
-
-
-    def __del__(self):
-        del PackageProcessingBase.instances[self.name]
-        
-       
-
-    # Baseclass implementation.
-    # Fail out.
-    def process_impl(self, 
-                     path,
-                     package_id,
-                     major, 
-                     minor, 
-                     patch, 
-                     command,
-                     size, 
-                     description,
-                     vendor,
-                     target): 
-        print "ERROR: PackagingProcessBase.process_impl(): Base class called"
-        assert(False)
-
-
-
-class ECU1ModuleLoaderProcessor(PackageProcessingBase):
-    def __init__(self):
-        super(ECU1ModuleLoaderProcessor, self).__init__("ecu1_loader")
-
-    def process_impl(self, 
-                     path,
-                     package_id,
-                     major, 
-                     minor, 
-                     patch, 
-                     command,
-                     size, 
-                     description,
-                     vendor,
-                     target): 
-        print "{}: Got process_impl() call".format(type(self).__name__)
-        print "  ID:     {}".format(package_id)
-        print "  ver:    {}.{}.{} ".format(major, minor, patch)
-        print "  cmd:    {}".format(command)
-        print "  size:   {}".format(size)
-        print "  descr:  {}".format(description)
-        print "  vendor: {}".format(vendor)
-        print "  target: {}".format(target)
-        return (0, "ECU1 flashing successful")
-
-
-class PackageManagerProcessor(PackageProcessingBase):
-    def __init__(self):
-        super(PackageManagerProcessor, self).__init__("package_manager")
-        pkg_mgr_bus_name = dbus.service.BusName("org.genivi.package_manager",
-                                                     bus=self.bus)
-        pkg_mgr_obj = self.bus.get_object("org.genivi.package_manager",
-                                          "/org/genivi/package_manager")
-
-        self.process_package_dbus = pkg_mgr_obj.get_dbus_method("process_package", 
-                                                                "org.genivi.package_manager")
-        self.get_installed_packages_dbus = pkg_mgr_obj.get_dbus_method("get_installed_packages", 
-                                                                       "org.genivi.package_manager")
-
-    
-    def process_impl(self, 
-                     path,
-                     package_id,
-                     major, 
-                     minor, 
-                     patch, 
-                     command,
-                     size, 
-                     description,
-                     vendor,
-                     target): 
-        print "{}: Got process_impl() call".format(type(self).__name__)
-        print "  ID:     {}".format(package_id)
-        print "  ver:    {}.{}.{} ".format(major, minor, patch)
-        print "  cmd:    {}".format(command)
-        print "  size:   {}".format(size)
-        print "  descr:  {}".format(description)
-        print "  vendor: {}".format(vendor)
-        print "  target: {}".format(target) 
-
-        # res = self.process_package_dbus(path,
-        #                                 package_id,
-        #                                 major, 
-        #                                 minor, 
-        #                                 patch, 
-        #                                 command,
-        #                                 size, 
-        #                                 description,
-        #                                 vendor,
-        #                                 target)
-        # return res
-        return (0, "Package installation successful")
-            
     
 #
 # SOTA Client-facing methods
@@ -276,6 +113,14 @@ class SLMService(dbus.service.Object):
                           description,
                           vendor,
                           target): 
+        def handle_dbus_reply(self, result):
+            print "RESULT: {}".format(result)
+            self.installation_report(package_id, major, minor, patch, res, res_desc)
+            
+        def handle_dbus_error(self, error):
+            print "ERROR: {}".format(result)
+            self.installation_report(package_id, major, minor, patch, 99, 'internal error')
+            
         print "Got download complete"
         print "  ID:     {}".format(package_id)
         print "  ver:    {}.{}.{} ".format(major, minor, patch)
@@ -287,23 +132,42 @@ class SLMService(dbus.service.Object):
         print "  target: {}".format(target)
         print "---"
 
-        # Locate and invoke the correct package processor 
-        # (ECU1ModuleLoaderProcessor.process_impl(), etc)
-        #
-        (res, res_desc) = PackageProcessingBase.process(path,
-                                                        package_id,
-                                                        major, 
-                                                        minor, 
-                                                        patch, 
-                                                        command,
-                                                        size, 
-                                                        description,
-                                                        vendor,
-                                                        target)
+        try:
+            print "1"
+            target_bus_name = dbus.service.BusName('org.genivi.'+target,
+                                                   bus=self.bus)
+            
+            print "2"
+            target_obj = self.bus.get_object(target_bus_name.get_name(), 
+                                             "/org/genivi/" + target)
 
+            print "3"
+            process_package = target_obj.get_dbus_method("process_package", 
+                                                         "org.genivi." + target)
 
-        self.installation_report(package_id, major, minor, patch, res, res_desc)
-        return True
+            #
+            # Locate and invoke the correct package processor 
+            # (ECU1ModuleLoaderProcessor.process_impl(), etc)
+            #
+            print "4"
+            (res, res_desc) = process_package(path,
+                                              package_id,
+                                              major, 
+                                              minor, 
+                                              patch, 
+                                              command,
+                                              size, 
+                                              description,
+                                              vendor,
+                                              target)
+            print "5"
+            self.installation_report(package_id, major, minor, patch, res, res_desc)
+            return True
+        except: 
+            print "Unexpected error:", sys.exc_info()[0]
+            self.installation_report(package_id, major, minor, patch, 99, sys.exc_info()[0])
+            return True
+
         
     @dbus.service.method("org.genivi.software_loading_manager")
     def get_installed_packages(self): 
@@ -323,8 +187,7 @@ class SLMService(dbus.service.Object):
 
 DBusGMainLoop(set_as_default=True)
 slm_sota = SLMService()
-package_manager = PackageManagerProcessor()
-ecu1_module_loader = ECU1ModuleLoaderProcessor()
+
 #
 #
 #
