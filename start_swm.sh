eb13f232cf9a3851ca48f5eb2e889719fc0d206c
Magnus Feuer <mfeuer@jaguarlandrover.com>
2015-11-01 17:48:17 -0800

    Added windows positioning to created xterms

--- start_swm.sh
+++ start_swm.sh
@@ -10,10 +10,18 @@
 
 # We use xterm since there is no way of putting gnome-terminal in the
 # foreground.
-xterm -T "Package Manager" -e python package_manager.py &
-xterm -T "Partition Manager" -e python partition_manager.py &
-xterm -T "ECU1 Module Loader" -e python ecu1_module_loader.py &
-xterm -T "Software Loading Manager" -e python software_loading_manager.py
-xterm -T "HMI" -e python hmi.py
-
+xterm -geometry 80x15+0+0 -T "Package Manager" -e python package_manager.py &
+xterm -geometry 80x15+0+300 -T "Partition Manager" -e python partition_manager.py &
+xterm -geometry 80x15+0+600 -T "ECU1 Module Loader" -e python ecu1_module_loader.py &
+xterm -geometry 80x24+800+000 -T "Software Loading Manager" -e python software_loading_manager.py &
+echo "Please run"
+echo
+echo "   python sota_client.py"
+echo
+echo "to start package use case."
+echo 
 echo "Press Ctrl-C shut down Software Manager"
+
+xterm -geometry 80x20+800+400 -T "HMI" -e python hmi.py
+
+case 
