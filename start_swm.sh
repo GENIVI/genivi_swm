52bce8749d36cd2b55064acc1438a95508e3af32
Magnus Feuer <mfeuer@jaguarlandrover.com>
2016-01-05 12:40:12 -0800

    Now uses gnome terminals instead of xterm.

--- start_swm.sh
+++ start_swm.sh
@@ -7,22 +7,39 @@
 # Ubuntu Software Manager Loader launch script
 #
 
+PID_FNAME=/tmp/swm_pidlist
 
-# We use xterm since there is no way of putting gnome-terminal in the
-# foreground.
-xterm -geometry 80x15+0+0 -T "Package Manager" -e "cd package_manager;python package_manager.py" &
-xterm -geometry 80x15+0+300 -T "Partition Manager" -e "cd partition_manaer; python partition_manager.py" &
-xterm -geometry 80x15+0+600 -T "ECU1 Module Loader" -e "cd module_loader; python ecu1_module_loader.py" &
-xterm -geometry 80x24+800+000 -T "Software Loading Manager" -e "cd software_loading_manager; python software_loading_manager.py" &
+killswm () {
+	for i in $(cat $PID_FNAME); do
+		kill $i
+	done
+	exit 0
+}
+
+rm -f $PID_FNAME
+gnome-terminal --geometry 80x15+0+0 -x bash -c "echo \$BASHPID >> $PID_FNAME; echo -ne \"\033]0;Package Manager\007\"; cd package_manager;python package_manager.py" 
+
+gnome-terminal --geometry 80x15+0+500 -x bash -c "echo \$BASHPID >> $PID_FNAME; echo -ne \"\033]0;Partition Manager\007\";cd partition_manager; python partition_manager.py" 
+
+gnome-terminal --geometry 80x15+0+1000 -x \bash -c "echo \$BASHPID >> $PID_FNAME; echo -ne \"\033]0;ECU1 Module Loader\007\"; cd module_loader_ecu1; python module_loader_ecu1.py" 
+
+gnome-terminal --geometry 80x15+0+1500  -x bash -c "echo \$BASHPID >> $PID_FNAME; echo -ne \"\033]0;Lifecycle Manager\007\";cd lifecycle_manager; python lifecycle_manager.py" 
+
+
+gnome-terminal --geometry 80x20+0+2500 -x bash -c "echo \$BASHPID >> $PID_FNAME; echo -ne \"\033]0;HMI\007\";cd hmi;python hmi.py" 
+
+gnome-terminal --geometry 80x24+0+2000 -x bash -c "echo \$BASHPID >> $PID_FNAME; echo -ne \"\033]0;Software Loading Manager\007\";cd software_loading_manager; python software_loading_manager.py" 
+
 echo "Please run"
 echo
-echo "   python sota_client.py"
+echo "   cd sota_client"
+echo "   sudo python sota_client.py -p test_package -i ../sample_update.upd -s xxx -d "Sample Update Description""
 echo
 echo "to start package use case."
 echo 
-echo "Press Ctrl-C shut down Software Manager"
+echo "Press Enter shut down Software Manager"
 
-xterm -geometry 80x20+800+400 -T "HMI" -e "cd hmi;python hmi.py"
+trap killswm INT
 
-
-
+read x
+killswm
